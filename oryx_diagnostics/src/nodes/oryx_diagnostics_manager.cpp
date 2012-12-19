fb704629c3ae7295f9fd14006cfa158f3ab5785d
apanzica <apanzica@wpi.edu>
2012-12-18 20:23:29 -0500

    Added the callback for processing the node_list command from oryx_diagnostics_client

--- oryx_diagnostics/src/nodes/oryx_diagnostics_manager.cpp
+++ oryx_diagnostics/src/nodes/oryx_diagnostics_manager.cpp
@@ -8,21 +8,38 @@
 #include<oryx_msgs/DiagnositicsRegistration.h>
 #include<oryx_diagnostics/DiagnosticsCommand.h>
 #include<boost/unordered/unordered_map.hpp>
-
+#include<boost/foreach.hpp>
 #include"NodeManager.h"
 
 using namespace oryx_diagnostics;
 
 class DiagnosticsManager{
+private:
+	typedef boost::function<bool (std::string& data, oryx_diagnostics::DiagnosticsCommand::Response& response)> command_func_;	///Typedef defining a boost::function object wrapper specifying the callback signature of commands
+	typedef boost::unordered_map<std::string, command_func_> command_map;
 public:
 	DiagnosticsManager(const std::string& name, ros::NodeHandle& nh):
 	name_(name),
 	nodes_(),
 	nh_(nh){
+		//Printout startup information
 		ROS_INFO_STREAM("Setting Up Oryx Diagnostics Manager <"<<name<<">...");
+
+		//Register services with ROS
 		this->reg_srv_ = this->nh_.advertiseService("oryx/diagnostics/registration", &DiagnosticsManager::regCB, this);
 		this->com_srv_ = this->nh_.advertiseService("oryx/diagnostics/commands", &DiagnosticsManager::comCB, this);
+
+		//Register itself with its node manager
 		this->nodes_.registerNode(name,"diagnostics_manager",-1,0,0);
+
+		//Register valid commands for communication with oryx_diagnostics_client
+		this->commands_["node_list"]=boost::bind(&DiagnosticsManager::listCB, this, _1, _2);
+
+		//Print out the supported command list
+		ROS_INFO("Registered Commands...");
+		BOOST_FOREACH(command_map::value_type item, this->commands_){
+			ROS_INFO("Registered Command: %s", item.first.c_str());
+		}
 
 	}
 	virtual ~DiagnosticsManager(){};
@@ -40,8 +57,8 @@ private:
 	ros::ServiceServer	reg_srv_;
 	ros::ServiceServer  com_srv_;
 
-	typedef boost::function<bool (std::string& data, oryx_diagnostics::DiagnosticsCommand::Response& response)> command_func_;	///Typedef defining a boost::function object wrapper specifying the callback signature of commands
-	boost::unordered_map<std::string, command_func_> commands_;			///boost::unordered_map mapping the string command name with its function
+
+	command_map commands_;			///boost::unordered_map mapping the string command name with its function
 
 	bool regCB(	oryx_msgs::DiagnositicsRegistration::Request& req,
 				oryx_msgs::DiagnositicsRegistration::Response& res){
@@ -72,8 +89,10 @@ private:
 
 	bool listCB(std::string& data, oryx_diagnostics::DiagnosticsCommand::Response& response){
 		std::stringstream res_data;
+		res_data<<"This is the current node list:\n";
 		this->nodes_.printList(res_data);
 		response.data = res_data.str();
+		response.success = true;
 		return true;
 	}
 };
